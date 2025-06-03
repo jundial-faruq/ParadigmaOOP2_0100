b161158b4fd197e2e5222b4e892d0109d08a9956
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-06-03 15:52:53 +0700

    membuat 3 class pewaris dan mejalankan

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -4,7 +4,6 @@ using namespace std;
 class orang {
     public :
         int umur;
-
         orang(int pUmur) :
             umur(pUmur)
             {
@@ -14,10 +13,38 @@ class orang {
 
 class pekerja : virtual public orang {
     public :
-    pekerja(int pUmur) :
+        pekerja(int pUmur) :
         orang(pUmur)
 
         {
             cout <<"pekerja dibuat\n" << endl;
         }
 };
+
+class pelajar : virtual public orang {
+    public :
+        pelajar(int pUmur) : 
+        orang(pUmur) {
+            cout << "Pelajar dibuat\n" << endl;
+        }
+};
+
+class budi : public pekerja, public pelajar {
+    public :
+
+        budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur) //hal ini dapat digunakan jika menggunakan virtual
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
+
+int main() {
+    budi a(12);
+
+
+    return 0;
+}
